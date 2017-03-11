84428080cf6e37a803682e2ce5210502b73221e1
David Max <davidkmax97@gmail.com>
2017-03-11 06:06:21 -0500

    mostly finished make tbale. still a few bugs

--- python_scripts/parsing_funs.py
+++ python_scripts/parsing_funs.py
@@ -12,13 +12,11 @@ file_extension = 'yelp_academic_dataset_{}.json'
 file_path = {}
 for name in ['business','checkin','review','tip','user']:
     file_path[name] = folder.format(file_extension.format(name))
-    
-insert_template = 'INSERT INTO {table} ({keys}) VALUES ({values});'
 
 # creates type dict where each key corrseponds to the dictionary with keys
 # type, options, and if type was a list, list type.     
-def set_type_dict(path):
-    with open(path, 'r') as file:
+def set_type_dict(file_name):
+    with open(file_path[file_name], 'r') as file:
         line = file.readline()
         print(line)
         obj = json.loads(line)
@@ -63,29 +61,45 @@ def data_size(data, data_type, max_size):
         return(max_size)
     
 def load_file(file_name, type_dict, table_name):
+    
     cnx = mysql.connector.connect(user='david', password='Pw',
                               host='localhost')
     cursor = cnx.cursor()
     cursor.execute('USE yelp')
+    insert_template = 'INSERT INTO {table} ({keys}) VALUES ({values});'
     
     with open(file_path[file_name]) as file:
         for line in file:
-            data = json.loads(line)
+            obj = json.loads(line)
+            data = {}
             for key in type_dict:
-                if (type_dict['item_type'] != 'int'):
-                    data[key] = '"{}"'.format(str(data[key]))
+                if (type_dict[key]['type'] != 'int'):
+                    data[key] = '"{}"'.format(str(obj[key]))
                 else:
-                    data[key] = str(data[key])
+                    data[key] = str(obj[key])
                 
             cursor.execute(insert_template.format(
-                    table=table_name,
-                    keys=",".join(type_dict.keys()),
-                    values=",".join(data.values())))
-                
-                
-            
-                    
-            
-            
-            
-       
+                    table  = table_name,
+                    keys   = ", ".join(type_dict.keys()),
+                    values = ", ".join(data.values())))
+    cnx.conector.close()
+
+
+def make_table(file_name, type_dict, table_name, options=''):
+    cnx = mysql.connector.connect(
+            user     = 'david', 
+            password = 'Pw', 
+            host     = 'localhost')
+    cursor = cnx.cursor()
+    cursor.execute('USE yelp')
+    columns_list = []
+    for key in type_dict:
+        columns_list.append('"{col_name}"" {col_type}{col_details}'.format(
+                col_name = key,
+                col_type = type_dict[key]['type'],
+                details  = type_dict[key]['options'])
+         
+    cursor.execute('CREATE TABLE {table} ({columns}{col_options});' .format(
+            table       = table_name,
+            columns     = ', '.join(columns_list),
+            col_options = options))
